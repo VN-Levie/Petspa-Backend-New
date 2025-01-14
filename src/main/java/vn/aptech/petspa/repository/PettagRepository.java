51c929fe7cd76b5a5a837be06079b9b889246001
Levie-PC <svpv.hotro@gmail.com>
2025-01-14 17:14:07 +0700

    Rename PettagRepository to PetTagRepository for consistency in naming conventions

--- src/main/java/vn/aptech/petspa/repository/PettagRepository.java
+++ src/main/java/vn/aptech/petspa/repository/PettagRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import vn.aptech.petspa.entity.PetPhoto;
 import vn.aptech.petspa.entity.PetTag;
 
-public interface PettagRepository extends JpaRepository<PetTag, Long> {
+public interface PetTagRepository extends JpaRepository<PetTag, Long> {
 
     Optional<PetTag> findByPetId(Long id);
 
